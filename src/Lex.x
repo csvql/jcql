4636c9e2bb66be17c2b8414b10705794400aad95
Dominik Tarnowski <dom@tdom.dev>
2021-04-15 17:52:58 +0100

    feat(lexer): add used keywords

--- src/Lex.x
+++ src/Lex.x
@@ -27,12 +27,15 @@ tokens :-
 <0> true { \p _ -> TBool True p}
 <0> false { \p _ -> TBool False p}
 
+<0> import|take|join|inner|cross|on|case|when|then|else|end { \p s -> TKeyword s p }
+
 <0> \(|\)|\[|\]  { mkBracket }
 <0> \.    { \p _ -> TDot p }
 <0> \,    { \p _ -> TComma p }
 <0> @single_str|@double_str { \p s -> TString (strip_quotes s) p}
 <0> @int                    { \p s -> TInt (read s) p }
 <0> @identifier             { \p s -> TIdentifier s p }
+
 
 {
 strip_quotes = tail . init
@@ -43,6 +46,7 @@ str (TDot _)        = "."
 str (TComma _)      = ","
 str (TString s _)     = "\"" ++ s ++ "\""
 str (TIdentifier c _) = c
+str (TKeyword c _) = c
 str (TBracket c _) = c
 str (TInt i _)        = show i
 
@@ -50,6 +54,7 @@ data Token =
   TIdentifier String AlexPosn
   | TDot AlexPosn
   | TComma AlexPosn
+  | TKeyword String AlexPosn
   | TOperator String AlexPosn
   | TBracket String AlexPosn
   | TString String AlexPosn
--- test/LexerSpec.hs
+++ test/LexerSpec.hs
@@ -1,53 +1,112 @@
-module LexerSpec (testLexer) where
+module LexerSpec
+  ( testLexer
+  ) where
 
-import Test.Tasty ( testGroup, TestTree )
-import qualified Test.Tasty.QuickCheck as QC
-import Test.Tasty.HUnit ( testCase, (@?=) )
-import Lex
-    ( alexScanTokens,
-      str,
-      AlexPosn(AlexPn),
-      Token(..) )
-import Data.Char ( toUpper, toLower )
+import           Data.Char                      ( toLower
+                                                , toUpper
+                                                )
+import           Lex                            ( AlexPosn(AlexPn)
+                                                , Token(..)
+                                                , alexScanTokens
+                                                , str
+                                                )
+import           Test.Tasty                     ( TestTree
+                                                , testGroup
+                                                )
+import           Test.Tasty.HUnit               ( (@?=)
+                                                , testCase
+                                                )
+import qualified Test.Tasty.QuickCheck         as QC
 
 testLexer :: TestTree
-testLexer =
-  testGroup
-    "Lexer"
-    [ testCase "ignores whitespace" (l " " @?= []),
-      testCase "ignores multiple whitespaces" (l "     " @?= []),
+testLexer = testGroup
+  "Lexer"
+  [ testCase "ignores whitespace"           (l " " @?= [])
+  , testCase "ignores multiple whitespaces" (l "     " @?= [])
+  , testGroup "lexes keywords" $ map
+    (\input -> testCase input (l input @?= [TKeyword input startPn]))
+    lexKeywords
+  ,
       -- (can be assumed to work as it's handelled by alex) calculates column, position and line number correctly
       -- what about SELECTFROM? (no space). This needs to be figured out and fixed
-      testGroup "lexes basic strings" $ map (\q -> testCase q $ l (q ++ "hello world" ++ q) @?= [TString "hello world" startPn]) lexStringQuotes,
-      testGroup "string quotes can be escaped within string" $ map (\q -> testCase (q ++ " can be escaped with \\" ++ q) $ l (q ++ "hello \\" ++ q ++ "world" ++ q) @?= [TString ("hello \\" ++ q ++ "world") startPn]) lexStringQuotes,
-      testGroup "lexes operators" $ map (\input -> testCase input (l input @?= [TOperator input startPn])) lexOperators,
-      testGroup "lexes brackets" $ map (\input -> testCase input (l input @?= [TBracket input startPn])) lexBrackets,
-      testGroup "lexes identifiers" [
+    testGroup "lexes basic strings" $ map
+    (\q ->
+      testCase q
+        $   l (q ++ "hello world" ++ q)
+        @?= [TString "hello world" startPn]
+    )
+    lexStringQuotes
+  , testGroup "string quotes can be escaped within string" $ map
+    (\q ->
+      testCase (q ++ " can be escaped with \\" ++ q)
+        $   l (q ++ "hello \\" ++ q ++ "world" ++ q)
+        @?= [TString ("hello \\" ++ q ++ "world") startPn]
+    )
+    lexStringQuotes
+  , testGroup "lexes operators" $ map
+    (\input -> testCase input (l input @?= [TOperator input startPn]))
+    lexOperators
+  , testGroup "lexes brackets" $ map
+    (\input -> testCase input (l input @?= [TBracket input startPn]))
+    lexBrackets
+  , testGroup
+    "lexes identifiers"
+    [
         -- TODO throws error on invalid identifier name, e.g. `SELECT %.1`
         -- TODO throws error when starts with a number or underscore
         -- TODO throws error when contains a:
         --  - dot
         --  - dash
-        testCase "alphabetic identifier" $ l "quickBrownFox" @?= [TIdentifier "quickBrownFox" startPn],
-        testCase "alphanumeric identifier" $ l "a1960b" @?= [TIdentifier "a1960b" startPn],
-        testCase "lexes methods" $ l ".a1960b()" @?= [TDot startPn, TIdentifier "a1960b" (AlexPn 1 1 2), TBracket "(" (AlexPn 7 1 8), TBracket ")" (AlexPn 8 1 9)]
-      ],
-      testCase "lexes dot" $ l "." @?= [TDot startPn],
-      testCase "lexes a comma" $ l "," @?= [TComma startPn],
-      testCase "lexes arrow function" (l "=>  " @?= [TArrow startPn])
+      testCase "alphabetic identifier"
+    $   l "quickBrownFox"
+    @?= [TIdentifier "quickBrownFox" startPn]
+    , testCase "alphanumeric identifier"
+    $   l "a1960b"
+    @?= [TIdentifier "a1960b" startPn]
+    , testCase "lexes methods"
+    $   l ".a1960b()"
+    @?= [ TDot startPn
+        , TIdentifier "a1960b" (AlexPn 1 1 2)
+        , TBracket "(" (AlexPn 7 1 8)
+        , TBracket ")" (AlexPn 8 1 9)
+        ]
     ]
+  , testCase "lexes dot" $ l "." @?= [TDot startPn]
+  , testCase "lexes a comma" $ l "," @?= [TComma startPn]
+  , testCase "lexes arrow function" (l "=>  " @?= [TArrow startPn])
+  ]
 
 -- random capitalization per character is applied to create a mixed-case string
 testRandomWordWithRandomCapitalization :: String -> Int -> Bool
 testRandomWordWithRandomCapitalization w a = l w == l randomlyCapitalized
-  where
-    n = a `mod` length w
-    randomlyCapitalized = [if i == n then toLower (w !! i) else toUpper (w !! i) | i <- [0 .. length w -1]]
-    expected = map toUpper w
+ where
+  n = a `mod` length w
+  randomlyCapitalized =
+    [ if i == n then toLower (w !! i) else toUpper (w !! i)
+    | i <- [0 .. length w - 1]
+    ]
+  expected = map toUpper w
 
-lexOperators = [ "=", "==", ">=", "<=", ">", "<", "not", "and", "or", "-", "+", "/" ]
 
-lexBrackets = [ "(", ")", "[", "]" ]
+lexKeywords =
+  [ "import"
+  , "take"
+  , "join"
+  , "inner"
+  , "cross"
+  , "on"
+  , "case"
+  , "when"
+  , "then"
+  , "else"
+  , "end"
+  ]
+
+
+lexOperators =
+  ["=", "==", ">=", "<=", ">", "<", "not", "and", "or", "-", "+", "/"]
+
+lexBrackets = ["(", ")", "[", "]"]
 
 lexStringQuotes = ["\"", "\'"]
 
