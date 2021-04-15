ee3f4ac3e5dcc668b846e83d4d1b82cfedc51621
Dominik Tarnowski <dom@tdom.dev>
2021-04-15 17:14:07 +0100

    feat: add AST

---
+++ src/AST.hs
@@ -0,0 +1,59 @@
+module AST where
+
+data Query = 
+  [Import] -- import (...)
+  Identifer -- take {{identifier}}
+  [Join] -- join (...)
+  Filter -- filter 
+  Select -- select
+
+data Import =
+  AliasedImport Identifier Location 
+  | UnaliasedImport Location
+
+-- TODO: Think about whether we need any more joins than this 
+data Join = 
+  Inner Identifer Expr
+  | Cross Identifer 
+
+type Location = String
+type Identifier = String
+type Filter = Expr
+type Select = [SelectItem]
+data SelectItem =
+  SelectExpr Expr
+  | QualifiedWildcard Identifier
+  | Wildcard
+
+data Expr =
+  TableColumn String Int-- identifier, such as table name
+  | ValueExpr Value -- literal value
+  | BinaryOpExpr Expr BinaryOpType Expr
+  | UnaryOpExpr UnaryOpType Expr
+  | Function String [Expr] -- e.g. COALESCE(p.1, q.1)
+  | Case [(Expr, Expr)] Expr -- CASE statement, https://www.w3schools.com/sql/sql_case.asp
+    deriving (Show,Eq)
+
+data Value = 
+    ValueString String
+    | ValueInt Int
+    | ValueBool Bool
+    deriving (Show,Eq)
+
+
+data BinaryOpType =
+  EQ
+  | LT -- less than, <
+  | LEQ -- less than or equal to, <=
+  | GT -- greater than, >
+  | GEQ -- greater than or equal to, >=
+  | AND
+  | OR
+  | Sum -- +
+  | Difference -- -
+  | Product -- multiply, *
+  deriving (Show,Eq)
+
+data UnaryOpType =
+  NOT
+  deriving (Show,Eq)
