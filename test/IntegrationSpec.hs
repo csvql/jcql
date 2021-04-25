09f42181cfcc4ad0fc3d6c76a926c3a1998ed83c
Dominik Tarnowski <dom@tdom.dev>
2021-04-25 14:32:45 +0100

    test: eval to return an array

--- test/IntegrationSpec.hs
+++ test/IntegrationSpec.hs
@@ -11,16 +11,22 @@ import           Parse
 
 import           Data.Map
 
-eval :: String -> IO (Result Table)
-eval = evalRoot . parseJCQL . alexScanTokens
+eval :: String -> IO (Result [[String]])
+eval s = do
+  table <- (evalRoot . parseJCQL . alexScanTokens) s
+  return $ singleTable table
+
+singleTable :: Result Table -> Result [[String]]
+singleTable res = do
+  Prelude.map (head . elems) <$> res
 
 testIntegration :: TestTree
 testIntegration = testGroup
   "Integration"
   [ testCase "pro developers" $ do
-      out <- eval "import a './testCsv/user.csv' take a where not (a.2 = 'Aleksei') select a.2"
-      out @?= Ok
-        [ fromList [("a", ["Ryan"])]
-        , fromList [("a", ["Dom"])]
-        ]
+      out <-
+        eval
+          "import a './testCsv/user.csv' take a where not (a.2 = 'Aleksei') select a.2, a.3"
+      out @?= Ok [["Ryan", "GB"], ["Dom", "PL"]]
   ]
+
