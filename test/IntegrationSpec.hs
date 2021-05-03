2aaf7750023fb855c37917c9754cf2392f4a7ca6
Dominik Tarnowski <dom@tdom.dev>
2021-05-03 13:44:44 +0100

    test: problem 8.1

--- test/IntegrationSpec.hs
+++ test/IntegrationSpec.hs
@@ -53,6 +53,7 @@ testIntegration = testGroup
     , problem' "5.1"
     , problem' "6.1"
     , problem' "7.1"
+    , problem' "8.1"
     ]
   , testGroup "Example tests" [example "multiple joins", example "alias", example "left join", example "left join empty"]
   , testGroup
